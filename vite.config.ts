a361c20db2c4ec9b83aac4a336f97093cf9272ae
wangkaixing <15055142531@163.com>
2025-05-21 15:50:53 +0800

    feat: 打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(async () => ({
   },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
+  base: './',
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
